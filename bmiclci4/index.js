78dc1348ad1dd4e78a784b98cd96c69bbe8b3add
STEPHEN ONDEYO <ondeyostephen30@gmail.com>
2022-11-11 15:39:07 +0300

    Add files via upload

---
+++ bmiclci4/index.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+function bmiCalci() {
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+    const output = document.getElementById("result");
+
+    let bmi = (weight / ((height*height) / 10000)).toFixed(1)
+
+if (bmi < 18.5) {
+    output.innerText = bmi + ":You are underweight"
+}else if (bmi > 18.6 && bmi < 24.9){
+    output.innerText = bmi + ":You are of healthy weight"
+}else if (bmi > 25 && bmi < 29.9){
+    output.innerText = bmi + ":You are overweight"
+}else {
+    output.innerText = bmi + ":You are obese"
+}
+
+}
